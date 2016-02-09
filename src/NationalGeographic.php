570e12f8045fbe735c1e5388e79d3e2f650922fb
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-02-09 18:23:56 +0200

    Initial document parser.

--- src/NationalGeographic.php
+++ src/NationalGeographic.php
@@ -24,6 +24,14 @@ class NationalGeographic extends Provider
     /**
      * @inheritdoc
      */
+    public function __construct()
+    {
+        parent::__construct('nationalgeographic');
+    }
+
+    /**
+     * @inheritdoc
+     */
     protected function remote($date = null)
     {
         if ($date !== null) {
@@ -31,9 +39,46 @@ class NationalGeographic extends Provider
         }
         
         $html = $this->httpRequest(self::URL);
-        $document = new DOMDocument();
-        $document->loadHTML($html);
+        $document = $this->createDomDocument($html);
+        
+        $pod = $this->createPod();
+        $pod->title = $this->getPictureTitle($document);
+        $pod->imageUrl = $this->getPictureUrl($document);
+        $pod->desc = $this->getPictureInfo($document);
+        $pod->baseUrl = $this->getCanonicalUrl($document);
+        
+        return $pod;
+    }
+    
+    protected function getPictureTitle(DOMDocument $document)
+    {
+        if (!($elem = $document->querySelector('#page_head h1'))) {
+            throw new ProviderException('Picture title is missing.');
+        }
+        return $elem->textContent;
+    }
+    
+    protected function getPictureUrl(DOMDocument $document)
+    {
+        if (!($elem = $document->querySelector('#content_top .primary_photo img'))) {
+            throw new ProviderException('Picture is missing');
+        }
+        $src = $elem->getAttribute('src');
+        if (strpos($src, '//') === 0) {
+            $src = 'http:' . $src;
+        }
+        return $src;
     }
 
+    protected function getPictureInfo(DOMDocument $document)
+    {
+        $elem = $document->querySelector('#caption');
+        return '';
+    }
+    
+    protected function getCanonicalUrl(DOMDocument $document)
+    {
+    }
+    
 }
 
