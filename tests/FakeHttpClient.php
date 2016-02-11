beff25685b4a6c704755cfeb4567b19f40f2a369
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-02-11 11:01:52 +0200

    Fix loading data file.

--- tests/FakeHttpClient.php
+++ tests/FakeHttpClient.php
@@ -19,8 +19,9 @@ class FakeHttpClient implements HttpClientInterface
     public function get($url)
     {
         $file = str_replace('http://', '', $url);
-        $file = substr($file, strpos($file, '/'));
+        $file = substr($file, 0, strpos($file, '/'));
         $file .= '.html';
+        $file = dirname(__FILE__) . '/files/' . $file;
         return file_get_contents($file);
     }
 
