fd8ac21ab2a7619bff825550619a127555be85dd
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-02-11 11:03:37 +0200

    Implemented Earth Science.

--- src/EarthScience.php
+++ src/EarthScience.php
@@ -36,7 +36,34 @@ class EarthScience extends Provider
      */
     protected function remote($date = null)
     {
+        $html = $this->httpRequest(self::URL);
+        $document = $this->createDomDocument($html);
         
+        $pod = $this->createPod();
+        
+        // Title and base url.
+        if (!($elem = $document->querySelector('#alpha-inner h3.entry-header a'))) {
+            throw new ProviderException('Cannot get title and base url.');
+        }
+        $pod->baseUrl = $elem->getAttribute('href');
+        $pod->title = $elem->textContent;
+
+        // Image url.
+        $elem = $document->querySelector('#alpha-inner div.entry-body a.asset-img-link');
+        if (!$elem) {
+            throw new ProviderException('Cannot get image element.');
+        }
+        $pod->imageUrl = $elem->getAttribute('href');
+        
+        // Optional. Description.
+        $divs = $document->querySelectorAll('#alpha-inner div.entry-body div');
+        if ($divs) {
+            foreach ($divs as $div) {
+                $pod->desc .= $div->textContent . PHP_EOL;
+            }
+        }
+        
+        return $pod;
     }
 
 }
