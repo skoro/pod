c759578a1439d0c4e35bf2dee99c7148971dd0ad
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-02-16 14:41:47 +0200

    Add object to string conversion and validation rules.

--- src/Pod.php
+++ src/Pod.php
@@ -8,6 +8,8 @@
 
 namespace skoro\pod;
 
+use LogicException;
+
 /**
  * Picture of the day entity.
  */
@@ -43,5 +45,37 @@ class Pod
      * @var string url to image resource (site page).
      */
     public $baseUrl;
+ 
+    /**
+     * Show image in string context.
+     */   
+    public function __toString()
+    {
+        return sprintf('"%s" [%s]', $this->title, $imageUrl);
+    }
+    
+    /**
+     * Validate POD fields.
+     *
+     * @throws LogicException
+     */
+    public function validate()
+    {
+        if (empty($this->date)) {
+            throw new LogicException('date required.');
+        }
+        
+        if (strptime($this->date, '%Y-%m-%d') === false) {
+            throw new LogicException('Date must be in format Y-m-d');
+        }
+        
+        if (empty($this->name)) {
+            throw new LogicException('Provider name required.');
+        }
+        
+        if (!preg_match('/^https?:\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)/i', $this->imageUrl)) {
+            throw new LogicException('Image url is not valid url.');
+        }
+    }
     
 }
