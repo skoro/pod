1125a85c60ce12bfa8ed7a3f25167953d511efe7
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-02-17 10:40:54 +0200

    Base url, date. Tests passed.

--- src/Euronews.php
+++ src/Euronews.php
@@ -42,32 +42,50 @@ class Euronews extends Provider
         $pod = $this->createPod();
         $pod->date = $this->getDateFromDocument($document, '#potd-list-wrap .new-row .date', '%d/%m/%Y');
         
-        $element = $document->getElementById('potd-wrap');
-        if (!$element) {
-            throw new ProviderException('Wrapper element not found.');
+        // Wrapper element for date and base url.
+        $element = $document->querySelector('#potd-list-wrap .new-row a.selected');
+        if ($element && $element->hasChildNodes()) {
+            // Create absolute url from relative.
+            $href = parse_url($element->getAttribute('href'));
+            if (empty($href['scheme'])) {
+                $url = parse_url(self::URL);
+                $pod->baseUrl = $url['scheme'] . '://' . $url['host'] . $href['path'];
+            }
+            // Search date element.
+            foreach ($element->childNodes as $child) {
+                if ($child->nodeType == XML_ELEMENT_NODE && 
+                        $child->getAttribute('class') == 'date') {
+                    $pod->date = $this->parseDate($child->textContent, '%d/%m/%Y');
+                    break;
+                }
+            }
         }
         
-        foreach ($element->childNodes as $child) {
-            if ($child->nodeType == XML_ELEMENT_NODE) {
-                switch ($child->tagName) {
-                    case 'h1' :
-                        if (empty($pod->title)) {
-                            $pod->title = $child->textContent;
-                        }
-                        break;
+        // Wrapper element for title, image url and description.
+        $element = $document->getElementById('potd-wrap');
+        if ($element && $element->hasChildNodes()) {
+            foreach ($element->childNodes as $child) {
+                if ($child->nodeType == XML_ELEMENT_NODE) {
+                    switch ($child->tagName) {
+                        case 'h1' :
+                            if (empty($pod->title)) {
+                                $pod->title = $child->textContent;
+                            }
+                            break;
 
-                    case 'div' :
-                        if ($child->getAttribute('id') == 'image-caption') {
-                            $pod->desc = $child->textContent;
-                        }
-                        break;
+                        case 'div' :
+                            if ($child->getAttribute('id') == 'image-caption') {
+                                $pod->desc = $child->textContent;
+                            }
+                            break;
 
-                    case 'img' :
-                        $pod->imageUrl = $child->getAttribute('src');
-                        if (($pos = strpos($pod->imageUrl, '?')) !== false) {
-                            $pod->imageUrl = substr($pod->imageUrl, 0, $pos);
-                        }
-                        break;
+                        case 'img' :
+                            $pod->imageUrl = $child->getAttribute('src');
+                            if (($pos = strpos($pod->imageUrl, '?')) !== false) {
+                                $pod->imageUrl = substr($pod->imageUrl, 0, $pos);
+                            }
+                            break;
+                    }
                 }
             }
         }
