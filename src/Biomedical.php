7cefd306f0f4299cf85ccc32ac7679ea7de31f06
Skorobogatko Alexei <skorobogatko.oleksii@gmail.com>
2016-02-24 10:55:51 +0200

    Biomedical implemented.

--- src/Biomedical.php
+++ src/Biomedical.php
@@ -29,16 +29,63 @@ class Biomedical extends Provider
     /**
      * @inheritdoc
      */
+    protected function parsePodBaseUrl(DOMDocument $document)
+    {
+        $meta = $document->getElementsByTagName('meta');
+        foreach ($meta as $element) {
+            if ($element->getAttribute('name') == 'og:url') {
+                return $element->getAttribute('content');
+            }
+        }
+    }
+    
+    /**
+     * @inheritdoc
+     */
     protected function finalizePod(Pod $pod, DOMDocument $document, $response)
     {
         $container = $document->querySelector('.container .leftCol .section.main_profile');
-        if (!$this->container) {
+        if (!$container) {
             throw new ProviderException('Container wrapper is missing in document.');
         }
         
         foreach ($container->childNodes as $child) {
             if ($child->nodeType != XML_ELEMENT_NODE) {
                 continue;
+            }
+            
+            switch ($child->tagName) {
+                case 'h1': // title
+                    $pod->title = $child->textContent;
+                    break;
+                    
+                case 'h5': // date
+                    $pod->date = $this->parseDate($child->textContent, '%d %B %Y');
+                    break;
+                    
+                case 'span': // image url
+                    if ($child->hasChildNodes()) {
+                        $img = $child->lastChild;
+                        if ($img->tagName == 'img') {
+                            $src = $img->getAttribute('src');
+                            if (($pos = strrpos($src, '?')) !== false) {
+                                $src = substr($src, 0, $pos);
+                            }
+                            $pod->imageUrl = $src;
+                        }
+                    }
+                    break;
+                    
+                case 'div': // description
+                    if (!$pod->desc && $child->hasChildNodes()) {
+                        foreach ($child->childNodes as $node) {
+                            if ($node->nodeType == XML_ELEMENT_NODE && $node->tagName == 'p') {
+                                $pod->desc .= $node->textContent . "\n";
+                            }
+                        }
+                        $pod->desc = trim($pod->desc);
+                    }
+                    break;
             }
         }
     }
--- tests/BiomedicalTest.php
+++ tests/BiomedicalTest.php
@@ -34,13 +34,19 @@ class BiomedicalTest extends PODTestCase
     public function testImageUrl()
     {
         $pod = $this->provider->getPod();
-        $this->assertEquals('src="http://s3.amazonaws.com/mrc-bpod-production/imgs/1786/profile/BPoD-Adventures_of_Titin_18.2.16.jpg"', $pod->imageUrl);
+        $this->assertEquals('http://s3.amazonaws.com/mrc-bpod-production/imgs/1786/profile/BPoD-Adventures_of_Titin_18.2.16.jpg', $pod->imageUrl);
     }
     
     public function testBaseUrl()
     {
         $pod = $this->provider->getPod();
         $this->assertEquals('http://bpod.mrc.ac.uk/archive/2016/2/18', $pod->baseUrl);
+    }
+    
+    public function testDesc()
+    {
+        $pod = $this->provider->getPod();
+        $this->assertEquals("Your heart beats, thanks in part to a protein called titin. Someone born with a titin gene shortened by mutation will have a weaker heart, a condition called dilated cardiomyopathy. Disease severity varies from patients needing heart transplants to others living with milder symptoms. Differences are down to where on the gene the mutation occurs, with mutations near the start of the gene causing milder symptoms. Scientists genetically engineered zebrafish to have differently located mutations and found that severe cardiomyopathy was linked to mutations near a ‘failsafe’ region of the gene. This region normally codes for a separate smaller version of titin, which helps alleviate symptoms when mutations happen near the start of the gene. But if mutations happen at or after the failsafe region, small titin is badly made and symptoms are severe. Pictured are zebrafish embryos expressing small (top) and normal titin (bottom) in the heart muscle (arrowed).\nWritten by Gaëlle Coullon", $pod->desc);
     }
     
 }
